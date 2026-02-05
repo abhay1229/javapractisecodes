dd56e8dd061539fa7c1a4e45a34fd5479ed754d9
Abhay Jadhav <your_github_email_here>
2026-02-05 17:04:34 +0530

    Initial commit

---
+++ src/Basic/Constructor.java
@@ -0,0 +1,16 @@
+package Basic;
+
+public class Constructor {
+	
+		Constructor(){
+			System.out.println("constructor banadiya");
+			
+		}
+		 public static void main(String[] args) {
+			Constructor t = new Constructor();
+			
+		}
+
+	}
+
+
---
+++ src/Basic/Constructor2.java
@@ -0,0 +1,5 @@
+package Basic;
+
+public class Constructor2 {
+
+}
