552f978c7fc746e66d0945bf615fb91117c60325
Abhay Jadhav <your_github_email_here>
2026-02-05 17:08:53 +0530

    initial commit

--- src/Basic/Constructor2.java
+++ src/Basic/Constructor2.java
@@ -1,5 +1,30 @@
 package Basic;
 
+
+
 public class Constructor2 {
+	
+		private int id;
+		private String name;
+		
+		public Constructor2 () {
+			id=1229;
+			name="abhay";
+		}
+		
+		public int getid() {
+			return id;
+		}
+		
+		public String getname() {
+			return name;
+		}
+		
+		public static void main(String[] args) {
+			Employe2 chu = new Employe2();  //ye he bsdka constructor
+			System.out.println(chu.getid());
+			System.out.println(chu.name);
+		}
+
 
 }
